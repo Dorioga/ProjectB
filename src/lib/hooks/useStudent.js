241620133fff8ac7794212f66f576c3b7e833dcc
Diego Hernan Aguilar Gomez <dhag100993@hotmail.com>
2025-11-06 20:06:10 -0500

    refactor: simplificar la definición de useStudent y mejorar la legibilidad

--- src/lib/hooks/useStudent.js
+++ src/lib/hooks/useStudent.js
@@ -1,13 +1,8 @@
 import { useContext } from "react";
 import { StudentContext } from "../context/StudentContext";
 
-export const useStudent = () => {
-  const context = useContext(StudentContext);
-
-  // ✅ ESTO está bien porque useContext ya se ejecutó
-  if (!context) {
-    throw new Error("useStudent debe ser usado dentro de StudentProvider");
-  }
-
-  return context;
-};
+export default function useStudent() {
+  const ctx = useContext(StudentContext);
+  if (!ctx) throw new Error("useStudent debe usarse dentro de StudentProvider");
+  return ctx;
+}
